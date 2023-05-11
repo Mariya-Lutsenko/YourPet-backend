7be03fdc2263c3fb10d1e8851c4f9de6fca129f0
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-05-11 14:49:27 +0300

    setting

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -6,39 +6,41 @@ const { schemas } = require("../../models/user");
 const router = express.Router();
 
 //signup
-router.post(
-  "/register",
-  validateBody(schemas.registerSchema),
-  ctrlWrapper(ctrl.register)
-);
 
-router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verify));
+// router.post(
+//   "/register",
+//   validateBody(schemas.registerSchema),
+//   ctrlWrapper(ctrl.register)
+// );
 
-router.post(
-  "/verify",
-  validateBody(schemas.emailSchema),
-  ctrlWrapper(ctrl.resendVerifyEmail)
-);
+// router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verify));
+
+// router.post(
+//   "/verify",
+//   validateBody(schemas.emailSchema),
+//   ctrlWrapper(ctrl.resendVerifyEmail)
+// );
 
 //signin
-router.post(
-  "/login",
-  validateBody(schemas.loginSchema),
-  ctrlWrapper(ctrl.login)
-);
-router.post(
-  "/refresh",
-  validateBody(schemas.refreshSchema),
-  ctrlWrapper(ctrl.refresh)
-);
-router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
+// router.post(
+//   "/login",
+//   validateBody(schemas.loginSchema),
+//   ctrlWrapper(ctrl.login)
+// );
+// router.post(
+//   "/refresh",
+//   validateBody(schemas.refreshSchema),
+//   ctrlWrapper(ctrl.refresh)
+// );
+// router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 
-router.post("/logout", authenticate, ctrlWrapper(ctrl.logout));
+// router.post("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
-router.patch(
-  "/avatars",
-  authenticate,
-  upload.single("avatar"),
-  ctrlWrapper(ctrl.updateAvatar)
-);
+// router.patch(
+//   "/avatars",
+//   authenticate,
+//   upload.single("avatar"),
+//   ctrlWrapper(ctrl.updateAvatar)
+// );
+
 module.exports = router;
