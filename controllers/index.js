7fc972d60393909cea34ebad0e3d317e9f16cfd3
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-05-14 12:26:15 +0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const auth = require("./auth");
 const users = require("./users");
 const notices = require("./notices");
+const services = require("./servicesSidebar");
 
 module.exports = {
   auth,
   users,
   notices,
+  services,
 };
