4d57f2812a116f1b0c30d40d53ac0425da8f958d
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-05-15 21:39:29 +0300

    fix swagger

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,7 +3,7 @@ const { User } = require("../../models");
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { accessToken: "", refreshToken: "" });
-  res.status(204).json({
+  res.json({
     message: "Logout success",
   });
 };
