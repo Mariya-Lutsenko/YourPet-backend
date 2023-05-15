8b6b574555243cbaded9a5be59a9e0c0f513ddb3
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-05-16 01:03:31 +0300

    done  getNews

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -1,8 +1,10 @@
 const { News } = require("../../models");
+const { HttpError } = require("../../helpers");
 
 const getNews = async (req, res) => {
   const { page = 1, limit = 6, search = "" } = req.query;
   const skip = (page - 1) * limit;
+
   let totalPages = 1;
   if (search === "") {
     const allnews = await News.find({});
@@ -11,10 +13,13 @@ const getNews = async (req, res) => {
     res.status(200).json({ news, totalPages });
   } else {
     const allSearchNews = await News.find({ $text: { $search: search } });
-    const news = await News.find({ $text: { $search: search } }).skip(skip);
+    const news = await News.find({ $text: { $search: search } })
+      .skip(skip)
+      .limit(limit);
     totalPages =
       allSearchNews.length === 0 ? 1 : Math.ceil(allSearchNews.length / limit);
     res.status(200).json({ news, totalPages });
   }
 };
+
 module.exports = getNews;
