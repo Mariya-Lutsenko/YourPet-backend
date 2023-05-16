abcb3420014b991ad29061ceb91b71340a332838
YelenaKudryk <kudrykolena@gmail.com>
2023-05-16 19:05:05 +0300

    fix

--- models/myPetSchema.js
+++ models/myPetSchema.js
@@ -25,8 +25,7 @@ const myPetSchema = new Schema({
     },
     imageURL: {
         type: String,
-        default:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        required: [true, 'Set image of your pet'],
     },
     comments: {
         type: String,
