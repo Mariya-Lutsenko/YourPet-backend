6a4baeb582a25b7ca9800fb0a24eb465a2004da6
MarynaAponasko <aponasko.mari@gmail.com>
2023-05-21 12:51:03 +0300

    update

--- controllers/users/getAllInfo.js
+++ controllers/users/getAllInfo.js
@@ -1,10 +1,22 @@
-const { ctrlWrapper } = require('../../utils')
-const { MyPet } = require('../../models')
+const { ctrlWrapper } = require("../../utils");
+const { MyPet } = require("../../models");
 
 const getAllInfo = async (req, res, next) => {
-    const { _id: owner, name, email, phone, birthday, city, imageURL } = req.user;
-    const pets = await MyPet.find({ owner }, "-createdAt -updatedAt -owner")
-    res.status(200).json({pets, user: {id:owner, name, email, phone, birthday, city, imageURL}});
-}
+  const { _id: owner, name, email, phone, birthday, city, imageURL } = req.user;
+  const pets = await MyPet.find({ owner }, "-createdAt -updatedAt -owner");
+  res.status(200).json({
+    pets,
+    user: {
+      id: owner,
+      name,
+      email,
+      phone,
+      birthday,
+      city,
+      imageURL,
+      favorite,
+    },
+  });
+};
 
-module.exports = {getAllInfo: ctrlWrapper(getAllInfo)}
+module.exports = { getAllInfo: ctrlWrapper(getAllInfo) };
