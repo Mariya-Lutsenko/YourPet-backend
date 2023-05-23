b0a5b17cf002922f9c716e75046478a020280500
MarynaAponasko <aponasko.mari@gmail.com>
2023-05-23 16:44:44 +0300

    upd

---
+++ controllers/notices/deleteOwnNoticeById.js
@@ -0,0 +1,22 @@
+const { HttpError } = require("../../helpers");
+const { Notices } = require("../../models");
+const { ctrlWrapper } = require("../../utils");
+
+const deleteOwnNoticeById = async (req, res) => {
+  const { id } = req.params; // id notice
+  const { _id } = req.user; // id user
+  const deletedNotices = await Notices.findOneAndDelete({
+    _id: id,
+    owner: _id,
+  });
+  if (deletedNotices) {
+    return res.json({
+      id: `${id}`,
+      message: `Notice with id ${id} successfully delete`,
+    });
+  } else {
+    throw HttpError(404, `Notices with ${id} not found`);
+  }
+};
+
+module.exports = { deleteOwnNoticeById: ctrlWrapper(deleteOwnNoticeById) };
